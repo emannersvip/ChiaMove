00283eac468abd5cacc910065b7949e4c5a9a1c6
emannersvip <edson.manners@gmail.com>
2021-06-16 08:07:15 -0400

    Fixed the path and added vnc directory

--- ubuntu_vnc.pp
+++ ubuntu_vnc.pp
@@ -1,29 +1,33 @@
 # https://linuxize.com/post/how-to-install-and-configure-vnc-on-ubuntu-20-04/
 
 package {'tigervnc-standalone-server': ensure => installed}
-#package {'tightvncserver': ensure => installed}
 package {'xfce4': ensure => installed}
 package {'xfce4-goodies': ensure => installed}
+
+file {'vnc_dir':
+  ensure => directory,
+  path   => '/home/emanners/.vnc',
+}
 
 file {'vnc_passwd':
   ensure => present,
   path   => '/home/emanners/.vnc/passwd',
   mode   => '600',
-  source => '/home/emanners/Documents/puppet_files/passwd',
+  source => '/home/emanners/Crypto/Chia/ChiaMove/puppet_files/passwd',
 }
 
 file {'vnc_xstartup':
   ensure => present,
   path   => '/home/emanners/.vnc/xstartup',
   mode   => '700',
-  source => '/home/emanners/Documents/puppet_files/xstartup',
+  source => '/home/emanners/Crypto/Chia/ChiaMove/puppet_files/xstartup',
 }
 
 file {'vnc_config':
   ensure => present,
   path   => '/home/emanners/.vnc/config',
   mode   => '700',
-  source => '/home/emanners/Documents/puppet_files/config',
+  source => '/home/emanners/Crypto/Chia/ChiaMove/puppet_files/config',
 }
 
 exec {'vncserver':
